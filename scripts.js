e5a1ae3f38653c485497f6a8c29cac2f01f8bc9f
rodolfomori <rod_dolpho@hotmail.com>
2023-03-16 21:14:13 -0300

    aula-3]

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagem(imagem){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
+
+
+
+
